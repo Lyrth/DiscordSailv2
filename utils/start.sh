4150196a8764847b3180e50ae30bebf434637723
Lyrthras <38674984+Lyrth@users.noreply.github.com>
2018-05-05 15:32:28 +0800

    * screen cmd addition
    Edit screen cmd for the ability to recontinue viewing output through -x sail

--- utils/start.sh
+++ utils/start.sh
@@ -5,7 +5,7 @@ JAVAPATH=`which java`
 JAVAARGS=-Xmx1G
 
 while true; do
-	screen -d -m $JAVAPATH $JAVAARGS -jar $JARNAME
+	screen -dmS sail $JAVAPATH $JAVAARGS -jar $JARNAME
 
 	if [ $? -eq 0 ] || [ $? -eq 2 ]; then
 		break
