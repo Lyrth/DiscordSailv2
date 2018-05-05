acbb656203a42e4bafe4242cee2a990f38e07fb9
Lyrthras <38674984+Lyrth@users.noreply.github.com>
2018-05-05 16:33:34 +0800

    *Add relative jarpath
    Because I'm lazy (kinda)

--- utils/start.sh
+++ utils/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+JARPATH=../target/
 JARNAME=JavaDiscordSAILv2.jar
 JAVAPATH=`which java`
 JAVAARGS=-Xmx1G
 
 while true; do
-	screen -dmS sail $JAVAPATH $JAVAARGS -jar $JARNAME
+	screen -dmS sail $JAVAPATH $JAVAARGS -jar $JARPATH$JARNAME
 
 	if [ $? -eq 0 ] || [ $? -eq 2 ]; then
 		break
