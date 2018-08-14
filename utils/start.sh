929441c74dff725424a91b7a7538afed4a7fa053
Lyrthras <38674984+Lyrth@users.noreply.github.com>
2018-08-14 15:42:49 +0800

    * Revert everything in start.sh (almost)
    *sigh*

--- utils/start.sh
+++ utils/start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-JARPATH=../target/
-JARNAME=JavaDiscordSAILv2.jar
+JARNAME=../target/JavaDiscordSAILv2.jar
 JAVAPATH=`which java`
 JAVAARGS=-Xmx1G
-ERR=0
 
 while true; do
-	screen -dmSL sail sh -c '$JAVAPATH $JAVAARGS -jar $JARPATH$JARNAME; echo $?'
-	ERR=`tail -1 screenlog.0`
-	if [ $ERR -eq 0 ] || [ $ERR -eq 2 ]; then
+	screen -d -m $JAVAPATH $JAVAARGS -jar $JARNAME
+	
+	if [ $? -eq 0 ] || [ $? -eq 2 ]; then
 		break
 	fi
 done
