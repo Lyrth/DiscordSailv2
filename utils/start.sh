eb03efac63ebc20122b612eb101c1e6bb46c2b74
Lyrthras <38674984+Lyrth@users.noreply.github.com>
2018-08-14 14:58:05 +0800

    * Re-revert changes, try to actually catch errcode

--- utils/start.sh
+++ utils/start.sh
@@ -4,11 +4,12 @@ JARPATH=../target/
 JARNAME=JavaDiscordSAILv2.jar
 JAVAPATH=`which java`
 JAVAARGS=-Xmx1G
+ERR=0
 
 while true; do
-	screen -d -m $JAVAPATH $JAVAARGS -jar $JARPATH$JARNAME
-
-	if [ $? -eq 0 ] || [ $? -eq 2 ]; then
+	screen -dmSL sail sh -c '$JAVAPATH $JAVAARGS -jar $JARPATH$JARNAME; echo $?'
+	ERR=`tail -1 screenlog.0`
+	if [ $ERR -eq 0 ] || [ $ERR -eq 2 ]; then
 		break
 	fi
 done
